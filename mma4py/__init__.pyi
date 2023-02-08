b3307ba50b46a1ad4e737101a62bb6fd88bd3255
Yicong Fu <fuyicong1996@gmail.com>
2023-02-08 16:51:20 -0500

    update pyi

--- mma4py/__init__.pyi
+++ mma4py/__init__.pyi
@@ -1,6 +1,5 @@
 from __future__ import annotations
 import numpy
-from mpi4py import MPI
 
 __all__ = ["Optimizer", "Problem"]
 
@@ -20,39 +19,83 @@ class Problem:
             c(x) <= 0
     """
 
-    def __init__(
-        self,
-        comm: MPI.Comm,
-        nvars: int,
-        nvars_l: int,
-        ncons: int,
-    ) -> None:
+    def __init__(self, comm, nvars, nvars_l, ncons) -> None:
+        """
+        Initializer.
+
+        Args:
+            comm: MPI.Comm
+            nvars: int, size of the global design vector x
+            nvars_l: int, size of the local portion of x at current MPI processor
+            ncons: number of constraints
+        """
         pass
-    def getVarsAndBounds(
-        self,
-        x: numpy.ndarray,
-        lb: numpy.ndarray,
-        ub: numpy.ndarray,
-    ) -> None:
+    def getVarsAndBounds(self, x0, lb, ub) -> None:
+        """
+        Set initial design, lower and upper bounds.
+
+        Args:
+            x0: 1d numpy array, initial design vector
+            lb: 1d numpy array, lower bounds
+            ub: 1d numpy array, upper bounds
+        """
         pass
-    def evalObjCon(
-        self,
-        x: numpy.ndarray,
-        cons: numpy.ndarray,
-    ) -> float:
+    def evalObjCon(self, x, cons) -> float:
+        """
+        Evaluate objective and constraints.
+
+        Args:
+            x: 1d numpy array, contains design variable at current optimization
+               iteration
+            cons: 1d numpy array, constraint values to be updated from user's
+                  implementation
+
+        Return:
+            objective value
+        """
         pass
-    def evalObjConGrad(
-        self,
-        x: numpy.ndarray,
-        g: numpy.ndarray,
-        gcon: numpy.ndarray,
-    ) -> None: ...
-    pass
+    def evalObjConGrad(self, x, g, gcon) -> None:
+        """
+        Evaluate objective and constraint gradients.
+
+        Args:
+            x: 1d numpy array, contains design variable at current optimization
+               iteration
+            g: 1d numpy array, objective gradient to be updated from user's
+                  implementation
+            gcon: 2d numpy array, gcon[i, :] contains gradient of i-th
+                  constraint, to be updated from user's implementation
+        """
+        pass
 
 class Optimizer:
-    def __init__(self, prob: Problem, log_name: str) -> None: ...
-    def optimize(self, niter: int) -> int: ...
-    def getOptimizedDesign(self) -> numpy.ndarray: ...
+    def __init__(self, prob, log_name) -> None:
+        """
+        Initialize MMA optimizer.
+
+        Args:
+            prob: Problem, the problem instance
+            log_name: string, output log file name
+        """
+        pass
+    def optimize(self, niter) -> int:
+        """
+        Execute optimization.
+
+        Args:
+            niter: int, number of iterations
+
+        Return:
+            error code, 0 is success
+        """
+        pass
+    def getOptimizedDesign(self) -> numpy.ndarray:
+        """
+        Get optimized design variable xopt.
+
+        Return:
+            1d numpy array, optimized x
+        """
     pass
 
 def _petsc_initialize() -> None:
